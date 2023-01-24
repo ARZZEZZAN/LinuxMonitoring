2414c72266e47236f71ad6f562ec3d04ff0e8398
Killer Albertha <killeral@va-i5.kzn.21-school.ru>
2023-01-24 10:07:19 +0300

    f

--- src/02/main.sh
+++ src/02/main.sh
@@ -2,39 +2,53 @@
 
 # Get Hostname
 HOSTNAME=$(hostname)
+echo "HOSTNAME: $HOSTNAME"
 
 # Get Timezone
 TIMEZONE=$(timedatectl | grep "Time zone" | awk '{print $3}')
 UTC=$(date +%z)
+echo "TIMEZONE: $TIMEZONE UTC $UTC"
 
 # Get Current User
 USER=$(whoami)
+echo "USER: $USER"
 
 # Get OS Type and Version
 OS=$(lsb_release -d | awk '{print $2, $3}')
+echo "OS: $OS"
 
 # Get Current Time
 DATE=$(date '+%d %B %Y %T')
+echo "DATE: $DATE"
 
 # Get Uptime
 UPTIME=$(uptime -p)
 UPTIME_SEC=$(uptime | awk '{print $3}' | sed 's/,//g')
+echo "UPTIME: $UPTIME"
+echo "UPTIME_SEC: $UPTIME_SEC"
 
 # Get IP, Mask and Gateway
 IP=$(ip route get 1 | awk '{print $NF;exit}')
 MASK=$(ifconfig | grep "netmask" | awk '{print $4}')
 GATEWAY=$(ip route | grep default | awk '{print $3}')
+echo "IP: $IP"
+echo "MASK: $MASK"
+echo "GATEWAY: $GATEWAY"
 
 # Get RAM Information
-RAM_TOTAL=$(sysctl -a | awk '/hw./' | '/mem/'| grep Mem | awk '{print $2}')
-RAM_USED=$(sysctl -a | awk '/hw./' | '/mem/'| grep Mem | awk '{print $3}')
-RAM_FREE=$(sysctl -a | awk '/hw./' | '/mem/'| grep Mem | awk '{print $4}')
+RAM_TOTAL=$(free -h | grep Mem | awk '{print $2}')
+RAM_USED=$(free -h | grep Mem | awk '{print $3}')
+RAM_FREE=$(free -h | grep Mem | awk '{print $4}')
+echo "RAM_TOTAL: $RAM_TOTAL"
+echo "RAM_USED: $RAM_USED"
+echo "RAM_FREE: $RAM_FREE"
 
 # Get Root Partition Information
 SPACE_ROOT=$(df -h / | awk '{print $2}' | tail -n 1)
 SPACE_ROOT_USED=$(df -h / | awk '{print $3}' | tail -n 1)
 SPACE_ROOT_FREE=$(df -h / | awk '{print $4}' | tail -n 1)
+echo "SPACE_ROOT: $SPACE_ROOT"
+echo "SPACE_ROOT_USED: $SPACE_ROOT_USED"
+echo "SPACE_ROOT_FREE: $SPACE_ROOT_FREE"
 
 # Output all
-
-
